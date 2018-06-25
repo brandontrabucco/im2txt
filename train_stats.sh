ef2e09209ff6f835aa53aa4a7faa8359156057e2
Brandon Trabucco <brandon@btrabucco.com>
2018-06-25 14:28:50 -0400

    Delete train_stats.sh

--- train_stats.sh
+++
@@ -1,6 +0,0 @@
-python2 im2txt/train_stats.py \
---input_file_pattern="/home/btrabucco/research/im2txt/im2txt/data/coco/train-?????-of-00256" \
---data_dump_dir="/home/btrabucco/research/im2txt/im2txt/eval/" \
---checkpoint_dir="/home/btrabucco/research/im2txt/im2txt/train/" \
---eval_dir="/home/btrabucco/research/im2txt/im2txt/eval" \
---num_eval_examples=500 \
